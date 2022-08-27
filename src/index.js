0804ab0a355648c9eb16010d61cbe05c2920f843
ihumentsev <a.ihumentsev@gmail.com>
2022-08-27 14:20:12 +0200

    add-fitch-input

--- src/index.js
+++ src/index.js
@@ -1,13 +1,45 @@
 import getEvens from './js/getEvents';
 import temlateCards from './templates/temlateCards.hbs';
-const inputEl = document.querySelector('.search-form');
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+//////////////////////////
+const searchingInput = document.querySelector('.start-searching');
+const countryInput = document.querySelector('.choose-country');
 const listItemEl = document.querySelector('.event-list');
+const DEBOUNCE_DELAY = 400;
 
-inputEl.addEventListener('click', onInputClick);
+searchingInput.addEventListener(
+  'input',
+  debounce(onInputClick, DEBOUNCE_DELAY)
+);
+
 async function onInputClick(event) {
-  const { _embedded } = await getEvens('', 'UK');
-  console.log(_embedded.events);
+  const eventSearch = event.target.value;
+  listItemEl.innerHTML = '';
+  if (eventSearch === '') {
+    return;
+  }
 
-  const listItem = _embedded.events.map(item => temlateCards(item)).join('');
-  listItemEl.insertAdjacentHTML('beforeend', listItem);
+  const response = await getEvens(`${eventSearch}`, '');
+
+  try {
+    if (response.page.totalPages === 0) {
+      listItemEl.innerHTML = '';
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
+
+    if (response._embedded.events.length > 0) {
+      Notiflix.Notify.success(
+        `Hooray! We found ${response._embedded.events.length} images.`
+      );
+      const listItem = response._embedded.events
+        .map(item => temlateCards(item))
+        .join('');
+      listItemEl.insertAdjacentHTML('beforeend', listItem);
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
