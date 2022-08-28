7b226dade3d10c5f4b0e850517f21948e75e7b5f
Maksym Lizohub <maksym.lizohub@gmail.com>
2022-08-28 12:06:49 +0300

    Update makeModal.js

--- src/js/makeModal.js
+++ src/js/makeModal.js
@@ -1,10 +1,14 @@
-import modal from './templates/modal.hbs';
+import modal from '../templates/modal.hbs';
 
 const eventList = document.querySelector('.js-event-list');
 const modalContainer = document.querySelector('.modal');
 
 const onOpenModal = async e => {
+  if (e.target.nodeName === 'UL') return;
   e.preventDefault();
+  const itemElId = e.target.closest('li').id;
+
+  console.log(e);
 };
 
 eventList.addEventListener('click', onOpenModal);
