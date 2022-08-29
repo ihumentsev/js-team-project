eefd4fd899fd945916490d69d8e104ad0a6c17e2
ihumentsev <a.ihumentsev@gmail.com>
2022-08-29 20:29:46 +0200

    add-fitch-county

--- src/js/getEvents.js
+++ src/js/getEvents.js
@@ -6,6 +6,6 @@ const DEFAULT_PAGE_SIZE = 16;
 export default async function getEvens(keyword, country, page) {
   const url = 'https://app.ticketmaster.com/discovery/v2/events.json';
   const apiKey = 'w7mmBkK7aLh5F5nZUPuvBPD23VXj8bAs';
-  const filter = `?apikey=${apiKey}&keyword=${keyword}&countryCode=${country}&city_ids&page=${page}&size=${DEFAULT_PAGE_SIZE}`;
+  const filter = `?apikey=${apiKey}&keyword=${keyword}&countryCode=${country}&city_ids&page=${page}&size=${DEFAULT_PAGE_SIZE}&sort=random`;
   return await axios.get(`${url}${filter}`).then(res => res.data);
 }
