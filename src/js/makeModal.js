3ec86755f8b09d468c7e3f84648a3703f1d57ef1
Maksym Lizohub <maksym.lizohub@gmail.com>
2022-08-29 11:15:34 +0300

    update modal

--- src/js/makeModal.js
+++ src/js/makeModal.js
@@ -3,9 +3,14 @@ import axios from 'axios';
 
 import getEvens from './getEvents';
 
-const eventList = document.querySelector('.js-event-list');
-const modalContainer = document.querySelector('.js-modal');
-const closeModalBtn = document.querySelector('.js-modal-close-btn');
+const refs = {
+  eventList: document.querySelector('.js-event-list'),
+  modalContainer: document.querySelector('.js-modal'),
+  closeModalBtn: document.querySelector('.js-modal-close-btn'),
+};
+
+refs.eventList.addEventListener('click', onOpenModal);
+/* refs.closeModalBtn.addEventListener('click', onCloseModalBtn); */
 
 /* async function getEventByID(eventId) {
   const url = 'https://app.ticketmaster.com/discovery/v2/events.json';
@@ -13,31 +18,6 @@ const closeModalBtn = document.querySelector('.js-modal-close-btn');
   const filter = `?apikey=${apiKey}&id=${eventId}`;
   return await axios.get(`${url}${filter}`).then(res => res.data);
 } */
-
-const closeFn = () => {
-  const close = () => {
-    modalContainer.classList.add('is-hidden');
-    modalContainer.innerHTML = '';
-    document.body.classList.remove('no-scroll');
-    removeEventListener();
-  };
-  const closeModalContainer = e => {
-    if (e.target.dataset.modal !== '') return;
-    close();
-  };
-  const onKeyDownEscape = e => {
-    if (e.code !== 'Escape') return;
-    close();
-  };
-  const removeEventListener = () => {
-    document.removeEventListener('keydown', onKeyDownEscape);
-    closeModalBtn.removeEventListener('click', close);
-    modalContainer.removeEventListener('click', closeModalContainer);
-  };
-  modalContainer.addEventListener('click', closeModalContainer);
-  document.addEventListener('keydown', onKeyDownEscape);
-  closeModalBtn.addEventListener('click', close);
-};
 
 function getIdOnClickEventImg(e) {
   eventId = e.target.id;
@@ -48,23 +28,20 @@ function getEventByID(eventId) {
   console.log(eventId);
 }
 
-const onOpenModal = async e => {
+async function onOpenModal(e) {
   if (e.target.nodeName !== 'IMG') return;
   getIdOnClickEventImg(e);
   getEventByID(eventId);
 
-  modalContainer.innerHTML = doModal();
-
-  modalContainer.classList.remove('is-hidden');
+  refs.modalContainer.innerHTML = doModal();
+  refs.modalContainer.classList.remove('is-hidden');
   document.body.classList.add('no-scroll');
-  closeModalBtn.addEventListener('click', onCloseModalBtn);
+  refs.closeModalBtn = document.querySelector('.js-modal-close-btn');
+  refs.closeModalBtn.addEventListener('click', onCloseModalBtn);
+}
 
-  /* const onCloseModalBtn = () => {
-    modalContainer.classList.add('is-hidden');
-    document.body.classList.remove('no-scroll');
-    closeModalBtn.removeEventListener('click', onCloseModalBtn);
-  }; */
-  closeFn();
-};
-
-eventList.addEventListener('click', onOpenModal);
+function onCloseModalBtn() {
+  refs.closeModalBtn.removeEventListener('click', onCloseModalBtn);
+  document.body.classList.remove('no-scroll');
+  refs.modalContainer.classList.add('is-hidden');
+}
