35cc9e5cdbfdfaa3ff8bb6480271341a7e94b531
Vitaliy Opanashchuk <booking.opanashchuk@gmail.com>
2022-08-30 22:26:27 +0300

    add styles on cards

--- src/js/choose-country-scroll.js
+++ src/js/choose-country-scroll.js
@@ -1,9 +1,30 @@
 const dropDownBtn = document.querySelector('.js-dropbtn');
 const ulDropDownEl = document.querySelector('.country__list');
+const dropDownItemEl = document.querySelector('.country__item');
 dropDownBtn.addEventListener('click', onBtnClickOpenDropDown);
 
-export default function onBtnClickOpenDropDown(event) {
-ulDropDownEl.classList.add('is-shown');
+function onBtnClickOpenDropDown(event) {
+    event.stopPropagation();
+  ulDropDownEl.classList.toggle('is-shown');
+  ulDropDownEl.addEventListener('click', onCoutryItemClickChoose);
+console.log(ulDropDownEl)
 
 }
-onBtnClickOpenDropDown();
+console.log(dropDownBtn.textContent)
+function onCoutryItemClickChoose (event) {
+    event.stopPropagation();
+     if (event.currentTarget == dropDownItemEl){
+        dropDownBtn.textContent = dropDownItemEl.textContent;
+        ulDropDownEl.classList.remove('is-shown');
+    }
+    console.log(dropDownItemEl.textContent)
+}
+
+window.onclick = function (event) {
+    event.stopPropagation();
+    if (event.currentTarget !== dropDownItemEl){
+        ulDropDownEl.classList.remove('is-shown');
+    }
+
+    
+}
