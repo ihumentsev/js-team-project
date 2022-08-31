1725923d46f77158e8d467b14fb4065616ff53fc
ihumentsev <a.ihumentsev@gmail.com>
2022-08-31 09:44:42 +0200

    .

--- src/js/makeModal.js
+++ src/js/makeModal.js
@@ -1,60 +1,71 @@
-import doModal from '../templates/modal.hbs';
-import axios from 'axios';
+const paginationEl = document.querySelector('#pagination');
 
-import getEvens from './getEvents';
+let currentPage = 0;
 
-const refs = {
+export function renderPagination(totalPages, renderEvents) {
+  paginationEl.innerHTML = '';
 
-  eventList: document.querySelector('.js-event-list'),
+  let startPos = currentPage - 2;
+  let renderCount = totalPages < 5 ? totalPages : 5;
 
-  modalContainer: document.querySelector('.js-modal'),
-  closeModalBtn: document.querySelector('.js-modal-close-btn'),
-};
+  if (totalPages > 6) {
+    if (currentPage <= 3) {
+      startPos = 0;
+    }
 
+    if (currentPage + 3 > totalPages) {
+      startPos = totalPages - 5;
+    }
+  } else {
+    startPos = 0;
+  }
 
-function getIdOnClickEventImg(e) {
-  eventId = e.target.id;
-  console.log(e.target.id);
+  if (currentPage > 3 && totalPages > 5) {
+    createButton(1, false, () => setCurrentPage(0, renderEvents));
+    createButton('...', false, () =>
+      setCurrentPage(currentPage - 3, renderEvents)
+    );
+  }
 
-refs.eventList.addEventListener('click', onOpenModal);
-/* refs.closeModalBtn.addEventListener('click', onCloseModalBtn); */
+  for (let i = startPos; i < startPos + renderCount; i++) {
+    createButton(i + 1, currentPage === i, () =>
+      setCurrentPage(i, renderEvents)
+    );
+  }
 
-/* async function getEventByID(eventId) {
-  const url = 'https://app.ticketmaster.com/discovery/v2/events.json';
-  const apiKey = 'w7mmBkK7aLh5F5nZUPuvBPD23VXj8bAs';
-  const filter = `?apikey=${apiKey}&id=${eventId}`;
-  return await axios.get(`${url}${filter}`).then(res => res.data);
-} */
-
-function getIdOnClickEventImg(e) {
-  eventId = e.target.id;
-
-  return eventId;
+  if (currentPage + 3 < totalPages && totalPages > 5) {
+    createButton('...', false, () =>
+      setCurrentPage(currentPage + 3, renderEvents)
+    );
+    createButton(totalPages, false, () =>
+      setCurrentPage(totalPages - 1, renderEvents)
+    );
+  }
 }
 
-function getEventByID(eventId) {
-  console.log(eventId);
+function createButton(content, active = false, handleClick) {
+  const liElement = document.createElement('li');
+  liElement.classList.add('pagination__page-button');
+  liElement.innerHTML = content;
+
+  if (active) {
+    liElement.classList.add('pagination__page-button_active');
+  }
+
+  if (handleClick) {
+    liElement.addEventListener('click', handleClick);
+  }
+
+  paginationEl.append(liElement);
 }
 
-
-async function onOpenModal(e) {
-  if (e.target.nodeName !== 'IMG') return;
-
-  getIdOnClickEventImg(e);
-  getEventByID(eventId);
-
-  refs.modalContainer.innerHTML = doModal();
-  refs.modalContainer.classList.remove('is-hidden');
-  document.body.classList.add('no-scroll');
-  refs.closeModalBtn = document.querySelector('.js-modal-close-btn');
-  refs.closeModalBtn.addEventListener('click', onCloseModalBtn);
+export function setCurrentPage(number, renderEvents) {
+  currentPage = number;
+  renderEvents(currentPage);
 }
 
-function onCloseModalBtn() {
-  refs.closeModalBtn.removeEventListener('click', onCloseModalBtn);
-  document.body.classList.remove('no-scroll');
-  refs.modalContainer.classList.add('is-hidden');
+export function clearPagination() {
+  paginationEl.innerHTML = '';
 }
 
-
-
+//new pagination
